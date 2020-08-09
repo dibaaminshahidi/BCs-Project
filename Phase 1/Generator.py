a59456789430f69fe308f75de4e4611a661860ff
Diba Aminshahidi <dibaaminshahidi@gmail.com>
2020-08-09 21:38:31 +0430

    Generator

--- Phase 1/Generator.py
+++ Phase 1/Generator.py
@@ -11,11 +11,14 @@ import cv2
 import numpy as np
  
 #Generate 
-for x in range(10):
+
+n = 20
+
+for x in range(n):
     img = Image.open("./../Assets/Raw.jpg")
     draw = ImageDraw.Draw(img)
     W, H = img.size
-    n = 0
+    r = 0
     number = ""
     left = W
     top = H
@@ -24,55 +27,81 @@ for x in range(10):
 
     for y in range(0,6):
         if (y==2):
-            n = "ب"
-            number = number + n
+            r = random.choice(['ط', 'د','س', 'ش', 'ک', 'ن', 'ل', 'ق', 'ص', 'ت', 'ب'])
+            number = number + r
         else:
-            n = random.randint(1,9)
-            number = number + persian.enToPersianNumb(n)
+            r = random.randint(1,9)
+            number = number + persian.enToPersianNumb(r)
      
     iran = "۱ ۱"
+    fontsize = 85
+    font = ImageFont.truetype("./../Fonts/STITRBD.TTF",fontsize)
+    draw.text((W-110, (H/2)-20),iran,(10,10,10),font=font,align="center")
+    
     fontsize = 95
-    font = ImageFont.truetype("./../Fonts/B Traffic Bold.ttf",fontsize)
+    font = ImageFont.truetype("./../Fonts/STITRBD.TTF",fontsize)
     w, h = font.getsize(number)
-    while(w>325):
+
+    # print(w)
+
+    while(w>330):
         fontsize = fontsize-1
-        font = ImageFont.truetype("./../Fonts/B Traffic Bold.ttf",fontsize)
+        font = ImageFont.truetype("./../Fonts/STITRBD.TTF",fontsize)
         w, h = font.getsize(number)
+
     draw.text((60, (H/2)-33),number,(10,10,10),font=font,align='center')
-    draw.text((W-110, (H/2)-23),iran,(10,10,10),font=font,align="center")
+    
     num = str(x)
-    img.save('./Output/Simple/Resault'+ num +'.jpg')
+    img.save('./Output/Simple/Resault '+ num +'.jpg')
+
 
     #BLUR
     im1 = img.filter(ImageFilter.BLUR)
-    im1.save('./Output/Noisy/Blur'+ num +'.jpg')
+    im1.save('./Output/Noisy/Blur/Blur '+ num +'.jpg')
  
     #SALT AND PEPPER
-    for y in range(4):
+
+    m = 4
+
+    for y in range(m):
         im2 = img
         snp = np.random.rand(H,W,3) * 255
         snp = Image.fromarray(snp.astype('uint8')).convert('L')
         mask = Image.new("L", im1.size,128)
         im2 = Image.composite(im2, snp , mask)
         Y = str(y)
-        im2.save('./Output/Noisy/Salt & Pepper'+ Y + num +'.jpg')
+        im2.save('./Output/Noisy/Salt & Pepper/SnP '+ Y + num +'.jpg')
 
         # BLUR AND SALT AND PEPPER
         im3 = im2.filter(ImageFilter.BLUR)
-        im3.save('./Output/Noisy/Blur with Salt & Pepper'+ Y + num +'.jpg')
+        im3.save('./Output/Noisy/Blur with Salt & Pepper/Mixed '+ Y + num +'.jpg')
         
-        for z in range(10):
-            n = random.randint(-45,45)
+        p = 10
+        
+        for z in range(p):
+            #Perspective
+            width, height = im3.size
+            a = random.uniform(-0.5,0.5)
+            xshift = abs(a) * width
+            new_width = width + int(round(xshift))
+            im4 = im3.transform((new_width, height), Image.AFFINE,
+                    (1, a, -xshift if a > 0 else 0, 0, 1, 0), Image.BICUBIC)
+            Z = str(z)
+            im4.save('./Output/Perspective/Perspective '+ Z + Y + num +'.jpg')
 
-            delta_w = 3*W 
+            #Rotate
+            n = random.randint(-45,45)
+            delta_w = W
             delta_h = 3*H
             padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
-            print(padding)
-            new_im = ImageOps.expand(im3, padding, fill="black")
+            # print(padding)
+            new_im = ImageOps.expand(im4, padding, fill="black")
             rotated = new_im.rotate(n)
-            Z = str(z)
-            rotated.save('./Output/Rotated/Rotated'+ Z + Y + num +'.jpg')
 
+            rotated.save('./Output/Rotated/Rotated '+ Z + Y + num +'.jpg')
+
+
+            
 
 
 
