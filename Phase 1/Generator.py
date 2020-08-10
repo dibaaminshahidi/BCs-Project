fe6bbd912c5926905b57c78980a422ddc9f751ff
Diba Aminshahidi <dibaaminshahidi@gmail.com>
2020-08-11 00:10:27 +0430

    Generator Done

--- Phase 1/Generator.py
+++ Phase 1/Generator.py
@@ -12,7 +12,7 @@ import numpy as np
  
 #Generate 
 
-n = 20
+n = 10
 
 for x in range(n):
     img = Image.open("./../Assets/Raw.jpg")
@@ -27,7 +27,9 @@ for x in range(n):
 
     for y in range(0,6):
         if (y==2):
-            r = random.choice(['ط', 'د','س', 'ش', 'ک', 'ن', 'ل', 'ق', 'ص', 'ت', 'ب'])
+            # r = random.choice(['ط', 'د','س', 'ش', 'ک', 'ن', 'ل', 'ق', 'ص', 'ت', 'ب'])
+            r = random.choice([ 'ن', 'ل', 'ق', 'ص', 'ت'])
+            l = r
             number = number + r
         else:
             r = random.randint(1,9)
@@ -41,53 +43,55 @@ for x in range(n):
     fontsize = 95
     font = ImageFont.truetype("./../Fonts/STITRBD.TTF",fontsize)
     w, h = font.getsize(number)
-
-    # print(w)
-
+    d = 33
+    #print(type(l))
+    #print(l)
+    t , h = font.getsize(l)
+    if(h > 80):
+        fontsize = fontsize-1
+        d = 40
     while(w>330):
         fontsize = fontsize-1
         font = ImageFont.truetype("./../Fonts/STITRBD.TTF",fontsize)
         w, h = font.getsize(number)
+        t, h = font.getsize(l)
+        if(h > 80):
+            d = 40
+        # print(h)
 
-    draw.text((60, (H/2)-33),number,(10,10,10),font=font,align='center')
+
+    draw.text((60, (H/2)-d),number,(10,10,10),font=font,align='center')
     
     num = str(x)
     img.save('./Output/Simple/Resault '+ num +'.jpg')
 
 
     #BLUR
-    im1 = img.filter(ImageFilter.BLUR)
-    im1.save('./Output/Noisy/Blur/Blur '+ num +'.jpg')
+    img1 = img.filter(ImageFilter.BLUR)
+    img1.save('./Output/Noisy/Blur/Blur '+ num +'.jpg')
  
-    #SALT AND PEPPER
-
+    #SALT AND PEPPER 
     m = 4
-
     for y in range(m):
-        im2 = img
+        img2 = img1
         snp = np.random.rand(H,W,3) * 255
         snp = Image.fromarray(snp.astype('uint8')).convert('L')
-        mask = Image.new("L", im1.size,128)
-        im2 = Image.composite(im2, snp , mask)
+        mask = Image.new("L", img1.size,128)
+        img2 = Image.composite(img2, snp , mask)
         Y = str(y)
-        im2.save('./Output/Noisy/Salt & Pepper/SnP '+ Y + num +'.jpg')
+        img2.save('./Output/Noisy/Salt & Pepper/SnP '+ Y + num +'.jpg')
 
-        # BLUR AND SALT AND PEPPER
-        im3 = im2.filter(ImageFilter.BLUR)
-        im3.save('./Output/Noisy/Blur with Salt & Pepper/Mixed '+ Y + num +'.jpg')
-        
+        #Perspective
         p = 10
-        
         for z in range(p):
-            #Perspective
-            width, height = im3.size
-            a = random.uniform(-0.5,0.5)
+            width, height = img2.size
+            a = random.uniform(-1,1)
             xshift = abs(a) * width
             new_width = width + int(round(xshift))
-            im4 = im3.transform((new_width, height), Image.AFFINE,
+            img4 = img2.transform((new_width, height), Image.AFFINE,
                     (1, a, -xshift if a > 0 else 0, 0, 1, 0), Image.BICUBIC)
             Z = str(z)
-            im4.save('./Output/Perspective/Perspective '+ Z + Y + num +'.jpg')
+            img4.save('./Output/Perspective/Perspective '+ Z + Y + num +'.jpg')
 
             #Rotate
             n = random.randint(-45,45)
@@ -95,7 +99,7 @@ for x in range(n):
             delta_h = 3*H
             padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
             # print(padding)
-            new_im = ImageOps.expand(im4, padding, fill="black")
+            new_im = ImageOps.expand(img4, padding, fill="black")
             rotated = new_im.rotate(n)
 
             rotated.save('./Output/Rotated/Rotated '+ Z + Y + num +'.jpg')
