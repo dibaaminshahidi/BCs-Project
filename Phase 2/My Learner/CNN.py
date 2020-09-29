abf51b743a9c54eb97cb885235a02913837c61e3
Diba Aminshahidi <dibaaminshahidi@gmail.com>
2020-09-29 18:59:14 +0330

    More Fonts

--- Phase 2/My Learner/CNN.py
+++ Phase 2/My Learner/CNN.py
@@ -68,108 +68,100 @@ def sw(argument):
 
 nb_classes = 25
 test = []
+FontList = [ f for f in os.listdir('./../../Fonts/') ]
 
-for c in ch:
-    path = './NumTrain/'+ c +'/'
-    for filename in os.listdir(path):
-        if(filename!='.DS_Store'):
-            im = Image.open(path+filename)
-            im = im.convert('L')
-            im = im.point(lambda x: 0 if x<128 else 255)
-            pix = np.array(im)
-            X.append(pix)
-            trainY.append(c)
+for f in FontList:
+    X = []
+    trainY =[]
+    x = []
+    testY =[]
+    if(f!='.DS_Store'):
+        print(f[:-4])
+        for c in ch:
+            path = './Train/'+f[:-4]+'/'+ c +'/'
+            for filename in os.listdir(path):
+                if(filename!='.DS_Store'):
+                    im = Image.open(path+filename)
+                    im = im.convert('L')
+                    im = im.point(lambda x: 0 if x<128 else 255)
+                    pix = np.array(im)
+                    X.append(pix)
+                    trainY.append(int(c))
 
-for c in ch:
-    path = './NumTest/'+ c +'/'
-    n = 0
-    for filename in os.listdir(path):
-        if(filename!='.DS_Store'):
-            im = Image.open(path+filename)
-            im = im.convert('L')
-            im = im.point(lambda x: 0 if x<128 else 255)
-            pix = np.array(im)
-            x.append(pix)
-            testY.append(c)
-            n=n+1
-    test.append(n)
-print(n)
+        for c in ch:
+            path = './Test/'+f[:-4]+'/'+ c +'/'
+            for filename in os.listdir(path):
+                if(filename!='.DS_Store'):
+                    im = Image.open(path+filename)
+                    im = im.convert('L')
+                    im = im.point(lambda x: 0 if x<128 else 255)
+                    pix = np.array(im)
+                    x.append(pix)
+                    testY.append(int(c))
 
+        X = np.array(X)
+        nsamples, nx, ny = X.shape
+        #trainX = X.reshape((nsamples,nx*ny))
+        trainX = X.reshape(nsamples, nx, ny, 1)
 
-X = np.array(X)
-nsamples, nx, ny = X.shape
-#trainX = X.reshape((nsamples,nx*ny))
-trainX = X.reshape(nsamples, nx, ny, 1)
-
-x = np.array(x)
-nsamples, nx, ny = x.shape
-#testX = x.reshape((nsamples,nx*ny))
-testX = x.reshape(nsamples, nx, ny, 1)
-
-
-trainY = np.array(trainY)
-testY = np.array(testY)
-
-
-label_encoder = LabelEncoder()
-label_encoder.fit(trainY)
-trainY = label_encoder.transform(trainY)
-testY = label_encoder.transform(testY)
+        x = np.array(x)
+        nsamples, nx, ny = x.shape
+        #testX = x.reshape((nsamples,nx*ny))
+        testX = x.reshape(nsamples, nx, ny, 1)
 
 
+        trainY = np.array(trainY)
+        testY = np.array(testY)
 
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=trainX.shape[1:4]))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-
-model.add(Flatten())
-model.add(Dense(64, activation='relu'))
-model.add(Dense(25))
+        # label_encoder = LabelEncoder()
+        # label_encoder.fit(trainY)
+        # trainY = label_encoder.transform(trainY)
+        # testY = label_encoder.transform(testY)
 
 
-model.compile(optimizer='adam',
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            metrics=['accuracy'])
 
 
-model.fit(trainX, trainY, epochs =1 ,validation_data=(testX, testY))
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=trainX.shape[1:4]))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(128, (3, 3), activation='relu'))
 
-score = model.evaluate(testX, testY, verbose=0)
+        model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(25))
 
-print("Accuracy: %.2f%%" % (score[1]*100))
 
-p = []
-p = model.predict_classes(testX)
-p = label_encoder.inverse_transform(p)
-y = label_encoder.inverse_transform(testY)
+        model.compile(optimizer='adam',
+                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    metrics=['accuracy'])
 
-i = 0 
-for (tl, l) in zip(y, p):
-    if (l != tl):
-        print("Label", sw(tl) ,"Pred:", sw(l))
-        i=i+1
-print (i)
 
-print(confusion_matrix(y,p))
+        model.fit(trainX, trainY, epochs =1 ,validation_data=(testX, testY))
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# cax = ax.matshow(confusion_matrix(y,p))
-# plt.title('Confusion matrix of the classifier')
-# fig.colorbar(cax)
-# ax.set_xticklabels([''] + ch)
-# ax.set_yticklabels([''] + ch)
-# plt.xlabel('Predicted')
-# plt.ylabel('True')
-# plt.show()
+        score = model.evaluate(testX, testY, verbose=0)
 
-f, ax = plt.subplots(figsize=(9, 9))
-cmap = sns.palplot(sns.diverging_palette(220, 200))
+        print("Accuracy: %.2f%%" % (score[1]*100))
+        
+        p = []
+        p = model.predict_classes(testX)
+        # p = label_encoder.inverse_transform(p)
+        # y = label_encoder.inverse_transform(testY)
 
-sns.heatmap(confusion_matrix(y,p), annot=True, fmt="d",cmap=cmap,linewidths=.5,cbar=0,xticklabels = ch, yticklabels=ch)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.show()
+        # i = 0 
+        # for (tl, l) in zip(y, p):
+        #     if (l != tl):
+        #         print("Label", sw(tl) ,"Pred:", sw(l))
+        #         i=i+1
+        # print (i)
+
+        print(confusion_matrix(testY,p))
+
+        cmap = sns.diverging_palette(260, 200,l= 90,s=60 ,as_cmap=True,sep=120)
+        sns.heatmap(confusion_matrix(testY,p), annot=True, fmt="d",cmap=cmap,linewidths=.5,cbar=1,xticklabels = ch, yticklabels=ch)
+        plt.title('Font:'+f[:-4]+' Accuracy: %.2f%%' % (score[1]*100))
+        plt.xlabel('Predicted')
+        plt.ylabel('True')
+        plt.savefig('./Confusion Matrix/'+f[:-4]+'.png')
+        plt.clf()  # Clear the figure for the next loop
